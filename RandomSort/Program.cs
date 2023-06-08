5362c6d3a71868dac6e6f1470eceea0c39114d2e
Kirill Gubin <kirill2000gubin@mail.ru>
2023-06-09 01:17:50 +0300

    Добавьте файлы проекта.

---
+++ RandomSort/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private static HttpClient httpClient = new HttpClient();
+    private static RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    private static string apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+
+    public static async Task Main(string[] args)
+    {
+        #region [ Генерация последовательности ]
+
+        List<int> numbers = randomNumberGenerator.GenerateRandomNumber(20, 100);
+        Console.WriteLine("Сгенерированная последовательность:\n");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(string.Join(" ", numbers) + "\n");
+        Console.ResetColor();
+        #endregion
+
+        #region [ Сортировка последовательности ]
+
+        string sortingAlgorithm = GetRandomSortAlgoritm();
+        randomNumberGenerator.SortNumbers(numbers, sortingAlgorithm);
+        Console.WriteLine("Отсортированная последовательность:\n");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(string.Join(" ", numbers) + "\n");
+        Console.ResetColor();
+
+        #endregion
+
+        #region [ Отправка  данных ]
+
+        Console.WriteLine("Отправить данные? (Y/N)\n" + 
+            $"( Ваш Url: {(apiUrl != null ? apiUrl : "*В файле appsettings.json отсутствует значение ApiUrl")} )");
+
+        bool isValidInput = false;
+
+        do
+        {
+            string input = Console.ReadLine()?.Trim()?.ToUpper();
+            
+            switch (input)
+            {
+                case "Y":
+                    await SendDataToApi(numbers);
+                    isValidInput = true;
+                    break;
+                case "N":
+                    Console.WriteLine("Отправка отменена\n");
+                    isValidInput = true;
+                    break;
+                case "S":
+                    Console.WriteLine("Завершение программы");
+                    return; 
+                default:
+                    Console.WriteLine("\nНекорректный ввод. Повторите попытку\n что бы закончить (S)\n");
+                    break;
+            }
+        } while (!isValidInput);
+
+        #endregion
+    }
+
+    private static string GetRandomSortAlgoritm()
+    {
+        string[] sortingAlgorithms = { "Bubble", "Gnome" };
+        int index = new Random().Next(sortingAlgorithms.Length);
+        return sortingAlgorithms[index];
+    }
+
+    private static async Task SendDataToApi(List<int> numbers)
+    {
+        try
+        {
+            var jsonContent = new StringContent(JsonConvert.SerializeObject(numbers), Encoding.UTF8, "application/json");
+
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, jsonContent);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine("Данные успешно отправлены");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при отправке: {ex.Message}");
+        }
+    }
+}
---
+++ RandomSort/RandomNumberGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class RandomNumberGenerator
+{
+
+    public List<int> GenerateRandomNumber(int minLength, int maxLength)
+    {
+
+        var random = new Random();
+
+        int length = random.Next(minLength, maxLength + 1);
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < length; i++)
+        {
+            numbers.Add(random.Next(-100, 101));
+        }
+
+        return numbers;
+    }
+
+    public void SortNumbers(List<int> numbers, string sortAlgorithm)
+    {
+        Console.WriteLine("Выполняется сортировка: " + sortAlgorithm + "\n");
+
+        Thread.Sleep(1500); 
+
+        DateTime startTime = DateTime.Now;
+
+        switch (sortAlgorithm)
+        {
+            case "Bubble":
+                BubbleSort(numbers);
+                break;
+            case "Gnome":
+                GnomeSort(numbers);
+                break;
+        }
+
+        DateTime endTime = DateTime.Now;
+        TimeSpan aTime = endTime - startTime;
+
+        Console.WriteLine("Время выполнения сортировки: " + aTime.TotalMilliseconds + " мс \n");
+
+    }
+
+    private void BubbleSort(List<int> numbers)
+    {
+        int temp; 
+
+        for(int i = 0; i < numbers.Count; i++)
+        {
+            for(int j = i+1; j < numbers.Count; j++)
+            {
+                if (numbers[j] < numbers[i])
+                {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+    }
+
+    private void GnomeSort(List<int> numbers)
+    {
+        int index = 0;
+        int temp;
+
+        while (index < numbers.Count)
+        {
+            if (index == 0 || numbers[index - 1] <= numbers[index])
+            {
+                index++;
+            }
+            else
+            {
+                temp = numbers[index];
+                numbers[index] = numbers[index - 1];
+                numbers[index - 1] = temp;
+                index--;
+            }
+        }
+    }
+}
